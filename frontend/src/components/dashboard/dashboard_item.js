a9e5a02cf2bd8e2f9d5cf95c0fb3f9e867836035
Bryan <blindaa121@gmail.com>
2020-05-13 09:14:47 -0700

    create dashboard index item

--- frontend/src/components/dashboard/dashboard_item.js
+++ frontend/src/components/dashboard/dashboard_item.js
@@ -6,8 +6,8 @@ const DashBoardItem = ({ activity }) => {
         <div className="event-item">
             <img className='event-img' src='event-img.jpg' />
             <br/>
-            <span className='event-date'>Event Date</span>
-            <span className='event-title'>Title</span>
+            <span className='event-date'>Wed, May 13 2020 2:30PM</span>
+            <span className='event-title'>Who's tryna get dunked on?</span>
         </div>
     )
 }
