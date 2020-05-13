fc8d4bc1c83dca4fcf3f36de7b3d1aa4b89b8bb3
miggs <jmigueldc95@gmail.com>
2020-05-12 21:19:20 -0700

    built activity show component skeleton

---
+++ frontend/src/components/activity/activity_container.jsx
@@ -0,0 +1,13 @@
+import Activity from "./activity_show";
+import { connect } from "react-redux";
+
+
+const mapStateToProps = state => ({
+
+});
+
+const mapDispatchToProps = dispatch => ({
+
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Activity);
