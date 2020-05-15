7755a25d1c6260bed3b994921a61426fd9b61716
Bryan <blindaa121@gmail.com>
2020-05-15 11:53:06 -0700

    Update activity index css

--- frontend/src/components/search/activity_index_item.jsx
+++ frontend/src/components/search/activity_index_item.jsx
@@ -9,7 +9,9 @@ const ActivityIndexItem = ({ activity }) => {
                 <span className='activity-date'>{activity.day}, {activity.date.slice(0,10)} {activity.time}</span>
                 <span className='activity-title'>{activity.title}</span>
                 <span className='activity-desc'>{activity.description}</span>
-                <Link className='see-more' to={`/activity/${activity._id}`}>See More Details</Link>
+                <div className='see-more-button'>
+                    <Link className='see-more-text' to={`/activity/${activity._id}`}>See More Details</Link>
+                </div>
             </div>
         </div>
             
