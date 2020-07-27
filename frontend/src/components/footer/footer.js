4839f77521fc9c84aa983d09946b79c054e7f033
ngocthily <ngoctly94@gmail.com>
2020-07-26 21:33:04 -0700

    Edit splash page images and made entire page responsive

--- frontend/src/components/footer/footer.js
+++ frontend/src/components/footer/footer.js
@@ -5,43 +5,45 @@ import { FaGithubAlt } from "react-icons/fa";
 
 const footer = props => {
     return (
-        <div className="footer">
+        <div className="footer-container">
+            <div className="footer">
 
-            <div className="huddleUP-repo"> 
-                <h1 className="engineer-name">huddleUP</h1>
-                <a href="https://github.com/jmiggs/huddleUP" target="_blank" rel="noopener noreferrer" className="huddleUP-icon"><FaGithubAlt /></a>
-            </div>
-
-            <div className="engineers-contacts-container"> 
-                <div className="contact-info-container"> 
-                    <h1 className="engineer-name">Bryan Linda</h1>
-                    <div className="symbol-icons-box"> 
-                        <a href="https://www.linkedin.com/in/bryan-linda-44389794/" target="_blank" rel="noopener noreferrer" className="footer-icon"><AiFillLinkedin /></a>
-                        <a href="https://github.com/blindaa121" target="_blank" rel="noopener noreferrer" className="footer-icon"><AiFillGithub /></a>
-                    </div>
+                <div className="huddleUP-repo"> 
+                    <h1 className="engineer-name">huddleUP</h1>
+                    <a href="https://github.com/jmiggs/huddleUP" target="_blank" rel="noopener noreferrer" className="huddleUP-icon"><FaGithubAlt /></a>
                 </div>
 
-                <div className="contact-info-container">
-                    <h1 className="engineer-name">Miguel DelaCruz</h1>
-                    <div className="symbol-icons-box"> 
-                        <a href="https://www.linkedin.com/in/jdelacruzm/" target="_blank" rel="noopener noreferrer" className="footer-icon"><AiFillLinkedin /></a>
-                        <a href="https://github.com/jmiggs" target="_blank" rel="noopener noreferrer" className="footer-icon"><AiFillGithub /></a>
+                <div className="engineers-contacts-container"> 
+                    <div className="contact-info-container"> 
+                        <h1 className="engineer-name">Bryan Linda</h1>
+                        <div className="symbol-icons-box"> 
+                            <a href="https://www.linkedin.com/in/bryan-linda-44389794/" target="_blank" rel="noopener noreferrer" className="footer-icon"><AiFillLinkedin /></a>
+                            <a href="https://github.com/blindaa121" target="_blank" rel="noopener noreferrer" className="footer-icon"><AiFillGithub /></a>
+                        </div>
                     </div>
-                </div>
 
-                <div className="contact-info-container"> 
-                    <h1 className="engineer-name">Ngoc Ly</h1>
-                    <div className="symbol-icons-box"> 
-                        <a href="https://www.linkedin.com/in/ngocthily/" target="_blank" rel="noopener noreferrer" className="footer-icon"><AiFillLinkedin /></a>
-                        <a href="https://github.com/ngocthily" target="_blank" rel="noopener noreferrer" className="footer-icon"><AiFillGithub /></a>
+                    <div className="contact-info-container">
+                        <h1 className="engineer-name">Miguel DelaCruz</h1>
+                        <div className="symbol-icons-box"> 
+                            <a href="https://www.linkedin.com/in/jdelacruzm/" target="_blank" rel="noopener noreferrer" className="footer-icon"><AiFillLinkedin /></a>
+                            <a href="https://github.com/jmiggs" target="_blank" rel="noopener noreferrer" className="footer-icon"><AiFillGithub /></a>
+                        </div>
                     </div>
-                </div>
-                
-                <div className="contact-info-container">
-                    <h1 className="engineer-name">Dorian Brown</h1>
-                    <div className="symbol-icons-box">
-                        <a href="https://www.linkedin.com/in/dorian-izaiah-brown-1430b3193/" target="_blank" rel="noopener noreferrer" className="footer-icon"><AiFillLinkedin /></a>
-                        <a href="https://github.com/DBsaiyan1321" target="_blank" rel="noopener noreferrer" className="footer-icon"><AiFillGithub /></a>
+
+                    <div className="contact-info-container"> 
+                        <h1 className="engineer-name">Ngoc Ly</h1>
+                        <div className="symbol-icons-box"> 
+                            <a href="https://www.linkedin.com/in/ngocthily/" target="_blank" rel="noopener noreferrer" className="footer-icon"><AiFillLinkedin /></a>
+                            <a href="https://github.com/ngocthily" target="_blank" rel="noopener noreferrer" className="footer-icon"><AiFillGithub /></a>
+                        </div>
+                    </div>
+                    
+                    <div className="contact-info-container">
+                        <h1 className="engineer-name">Dorian Brown</h1>
+                        <div className="symbol-icons-box">
+                            <a href="https://www.linkedin.com/in/dorian-izaiah-brown-1430b3193/" target="_blank" rel="noopener noreferrer" className="footer-icon"><AiFillLinkedin /></a>
+                            <a href="https://github.com/DBsaiyan1321" target="_blank" rel="noopener noreferrer" className="footer-icon"><AiFillGithub /></a>
+                        </div>
                     </div>
                 </div>
             </div>
--- frontend/src/components/splash/splash_page.js
+++ frontend/src/components/splash/splash_page.js
@@ -15,17 +15,19 @@ const goToSignUp = () => {
 
 const SplashPage = props => { 
     return (
-        <div>
+        <div className="splash-container">
 
             <div className="top-banner">
-                <div className="splash-nav">
-                    {/* <Link to="/" className="splash-nav-link">huddleUP</Link> */}
-                    <p id="splash-huddleup">huddleUP</p>
-                    <div className="auth-nav">
-                        <Link to="/login" id="login" className="splash-nav-link">Log In</Link>
-                        <Link to="/signup" id="signup" className="splash-nav-link">Sign Up</Link>
-                    </div>
-                </div> 
+                <div className="splash-nav-container">
+                    <div className="splash-nav">
+                        {/* <Link to="/" className="splash-nav-link">huddleUP</Link> */}
+                        <p id="splash-huddleup">huddleUP</p>
+                        <div className="auth-nav">
+                            <Link to="/login" id="login" className="splash-nav-link">Log In</Link>
+                            <Link to="/signup" id="signup" className="splash-nav-link">Sign Up</Link>
+                        </div>
+                    </div> 
+                </div>
                 <div className="top-banner-not-the-nav"> 
                     <div className="top-banner-text">
                         <h1 className="main-phrase">Nobody to play with? We got you covered.</h1>
@@ -34,70 +36,80 @@ const SplashPage = props => {
                             {/* <div className="signup-splash-button">
                                 <Link to="/signup" className="signup-splash-button-text">Sign Up, Have Fun</Link>
                             </div> */}
-                            <div className="signup-splash-button" onClick={goToSignUp}> 
-                                <p className="signup-splash-button-text">Sign Up, Have Fun</p>
+                            <div className="signup-splash-button-wrapper">
+                                <div className="signup-splash-button" onClick={goToSignUp}> 
+                                    <p className="signup-splash-button-text">Sign Up, Have Fun</p>
+                                </div>
                             </div>
                             <p className="signin-splash-text">Already using huddleUP? <Link to="/login" className="signin-splash-link">Log In</Link></p>
                         </div>
                     </div>
-                    <img className="top-banner-picture" src="cartoon-soccer-players.jpg" alt="Soccer Cartoon" />
+                    {/* <img className="top-banner-picture" src="cartoon-soccer-players.jpg" alt="Soccer Cartoon" /> */}
+                    <div className="top-banner-picture-container">
+                        <img className="top-banner-picture" src="tennis.png" alt="tennis"/>
+                    </div>
                 </div>
             </div>
+            <div className="bottom-half-of-splash-container">
+                <div className="bottom-half-of-splash">
+                    <h1 className="what-is-huddleup">What is huddleUP?</h1>
+                    <div className="huddleup-is"> 
 
-            <div className="bottom-half-of-splash">
-                <h1 className="what-is-huddleup">What is huddleUP?</h1>
-                <div className="huddleup-is"> 
-
-                    <div className="huddleup-is-one">
-                        <div className="huddleup-is-bottom-text-pic">
-                            <img className="huddleup-is-bottom-text-pic" src="bay-area.jpg" alt="bayareapic" />
+                        <div className="huddleup-is-one">
+                            <div className="huddleup-is-bottom-text-pic">
+                                {/* <img className="huddleup-one-img" src="bay-area.jpg" alt="bayareapic" /> */}
+                                <img className="huddleup-one-img" src="map.png" alt="map" />
+                            </div>
+                            <div className="huddleup-is-bottom-text">
+                                <h2>1. Find a loction</h2>
+                                <p>Pick a location near you for convenience.</p>
+                            </div>
                         </div>
-                        <div className="huddleup-is-bottom-text">
-                            <h2>1. Find a loction</h2>
-                            <p>Pick a location near you for convenience.</p>
+
+                        <div className="huddleup-is-two">
+                            <div className="huddleup-is-bottom-text-pic">
+                                {/* <img className="huddleup-two-img" src="subscribe-event.jpg" alt="subscribepic"/> */}
+                                <img className="huddleup-two-img" src="booked.png" alt="booked" />
+                            </div>
+                            <div className="huddleup-is-bottom-text">
+                                <h2>2. Host or Join</h2>
+                                <p>Found a fun event? Join it. Can't find any? Why not just host them.</p>
+                            </div>
+                        </div>
+
+                        <div className="huddleup-is-three">
+                            <div className="huddleup-is-bottom-text-pic">
+                                {/* <img className="huddleup-three-img" src="message.jpg" alt="messagepic"/> */}
+                                <img className="huddleup-three-img" src="calendar.png" alt="calendar" />
+                            </div>
+                            <div className="huddleup-is-bottom-text">
+                                <h2>3. Now or later</h2>
+                                <p>Tell everyone you're out right now, or make plans for the future. </p>
+                            </div>
+                        </div>
+
+                        <div className="huddleup-is-four">
+                            <div className="huddleup-is-bottom-text-pic">
+                                {/* <img className="huddleup-four-img" src="outside.jpg" alt="outsidepic" /> */}
+                                <img className="huddleup-four-img" src="basketball.png" alt="basketball" />
+                            </div>
+                            <div className="huddleup-is-bottom-text">
+                                <h2>4. Play</h2>
+                                <p>Go out and have some fun.</p>
+                            </div>
                         </div>
                     </div>
-
-                    <div className="huddleup-is-two">
-                        <div className="huddleup-is-bottom-text-pic">
-                            <img className="huddleup-is-bottom-text-pic" src="subscribe-event.jpg" alt="subscribepic"/>
-                        </div>
-                        <div className="huddleup-is-bottom-text">
-                            <h2>2. Host or Join</h2>
-                            <p>Found a fun event? Join it. Can't find any? Why not just host them.</p>
+                    {/* <div className="signup-bottom-button">
+                        <Link to="/signup" className="signup-bottom-button-text">Let's Go</Link>
+                    </div> */}
+                    <div className="signup-bottom-button-container">
+                        <div className="signup-bottom-button" onClick={goToSignUp}>
+                            <p className="signup-bottom-button-text">Let's Go</p>
                         </div>
                     </div>
-
-                    <div className="huddleup-is-three">
-                        <div className="huddleup-is-bottom-text-pic">
-                            <img className="huddleup-is-bottom-text-pic-except" src="message.jpg" alt="messagepic"/>
-                        </div>
-                        <div className="huddleup-is-bottom-text">
-                            <h2>3. Now or later</h2>
-                            <p>Tell everyone you're out right now, or make plans for the future. </p>
-                        </div>
-                    </div>
-
-                    <div className="huddleup-is-four">
-                        <div className="huddleup-is-bottom-text-pic">
-                            <img className="huddleup-is-bottom-text-pic" src="outside.jpg" alt="outsidepic" />
-                        </div>
-                        <div className="huddleup-is-bottom-text">
-                            <h2>4. Play</h2>
-                            <p>Go out and have some fun.</p>
-                        </div>
-                    </div>
-                </div>
-                {/* <div className="signup-bottom-button">
-                    <Link to="/signup" className="signup-bottom-button-text">Let's Go</Link>
-                </div> */}
-                <div className="signup-bottom-button" onClick={goToSignUp}>
-                    <p className="signup-bottom-button-text">Let's Go</p>
                 </div>
             </div>
-
             <Footer />
-            
         </div>
     )
 }
