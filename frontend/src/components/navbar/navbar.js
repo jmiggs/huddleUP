f159f626706bb05bb0e00d74198550862fa7637c
Dorian Brown <devorian143@gmail.com>
2021-01-02 19:13:56 -0800

    Navbar to Hooks

--- frontend/src/components/navbar/navbar.js
+++ frontend/src/components/navbar/navbar.js
@@ -144,31 +144,19 @@
 
 
 
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import EventsDropdown from './events_dropdown';
 import "../../reset.css";
 import "./navbar.css"
 
-class NavBar extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = { slide: false };
+const NavBar = props => {
+    const [slide, setSlide] = useState(false);
 
-        this.logoutUser = this.logoutUser.bind(this);
-        // this.getLinks = this.getLinks.bind(this);
-        this.huddleUpTab = this.huddleUpTab.bind(this);
-        this.myEventsTab = this.myEventsTab.bind(this);
-        this.exploreTab = this.exploreTab.bind(this);
-        this.hostTab = this.hostTab.bind(this);
-        this.profileTab = this.profileTab.bind(this);
-        this.slideIn = this.slideIn.bind(this);
-    }
-
-    logoutUser(e) {
+    const logoutUser = e => {
         e.preventDefault();
-        this.props.logout();
-    }
+        props.logout();
+    };
 
     // I'm going to make this responsive
 
@@ -193,94 +181,78 @@ class NavBar extends React.Component {
     //     }
     // }
 
-    slideIn() {
-        this.setState({ slide: !this.state.slide })
-    }
+    const slideIn = () => {
+        setSlide(!slide);
+    };
 
-    huddleUpTab() {
-        return (
-            <Link to="/" className="navbar-name">
-                huddleUP
-            </Link>
-        )
-    }
+    const huddleUpTab = () => (
+        <Link to="/" className="navbar-name">
+            huddleUP
+        </Link>
+    );
 
-    myEventsTab() {
-        return (
-            <Link to={`/users/${this.props.id}/myevents`} className="navbar-explore-link">
-                My Events
-            </Link>
-        )
-    }
+    const myEventsTab = () => (
+        <Link to={`/users/${props.id}/myevents`} className="navbar-explore-link">
+            My Events
+        </Link>
+    );
 
-    exploreTab() {
-        return (
-            <Link to="/" className="navbar-explore-link">
-                Explore
-            </Link>
-        )
-    }
+    const exploreTab = () => (
+        <Link to="/" className="navbar-explore-link">
+            Explore
+        </Link>
+    );
 
-    hostTab() {
-        return (
-            <Link to="/activity/host" className="navbar-host-link">
-                Host
-            </Link>
-        )
-    }
+    const hostTab = () => (
+        <Link to="/activity/host" className="navbar-host-link">
+            Host
+        </Link>
+    );
 
-    profileTab() {
-        return (
-            <Link to={`/users/${this.props.id}`} className="navbar-profile-link">
-                Profile
-            </Link>
-        )
-    }
+    const profileTab = () => (
+        <Link to={`/users/${props.id}`} className="navbar-profile-link">
+            Profile
+        </Link>
+    );
 
-
-
-    render() {
-        // if (this.state.slide) document.querySelector("body").classList.add("no-scroll");
-        // if (!this.state.slide) document.querySelector("body").classList.remove("no-scroll");
-
-        return (
-            <div className="navbar">
-                <div className="left-navbar">
-                    {this.huddleUpTab()}
-                </div>
-                <div className="right-navbar">
-                    {
-                        (this.state.slide) ?
-                            <div className="navbar-links slide">
-                                {this.myEventsTab()}
-                                {this.exploreTab()}
-                                {this.hostTab()}
-                                {this.profileTab()}
-                            </div> :
-                            <div className="navbar-links">
-                                {this.myEventsTab()}
-                                {this.exploreTab()}
-                                {this.hostTab()}
-                                {this.profileTab()}
-                            </div>
-                    }
-                    {this.state.slide ?
-                        <div className="burger cross" onClick={this.slideIn}>
-                            <div className="burger-line-1"></div>
-                            <div className="burger-line-2"></div>
-                            <div className="burger-line-3"></div>
-                        </div>
-                        :
-                        <div className="burger" onClick={this.slideIn}>
-                            <div className="burger-line-1"></div>
-                            <div className="burger-line-2"></div>
-                            <div className="burger-line-3"></div>
-                        </div>
-                    }
-                </div>
+    return (
+        <div className="navbar">
+            <div className="left-navbar">
+                {huddleUpTab()}
             </div>
-        )
-    }
+            <div className="right-navbar">
+                {
+                slide ?
+                    <div className="navbar-links slide">
+                        {myEventsTab()}
+                        {exploreTab()}
+                        {hostTab()}
+                        {profileTab()}
+                    </div> :
+                    <div className="navbar-links">
+                        {myEventsTab()}
+                        {exploreTab()}
+                        {hostTab()}
+                        {profileTab()}
+                    </div>
+                }
+                {
+                slide ?
+                    <div className="burger cross" onClick={slideIn}>
+                        <div className="burger-line-1"></div>
+                        <div className="burger-line-2"></div>
+                        <div className="burger-line-3"></div>
+                    </div>
+                    :
+                    <div className="burger" onClick={slideIn}>
+                        <div className="burger-line-1"></div>
+                        <div className="burger-line-2"></div>
+                        <div className="burger-line-3"></div>
+                    </div>
+                }
+            </div>
+        </div>
+    )
 }
 
 export default NavBar;
